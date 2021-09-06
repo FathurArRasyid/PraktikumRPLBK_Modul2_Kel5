f0be232e8bd911a62cc705fa52e721c031b1fee7
FathurArRasyid <50971647+FathurArRasyid@users.noreply.github.com>
2021-09-06 15:44:31 +0700

    perc 1

---
+++ modul2-kel5/src/index.js
@@ -0,0 +1,12 @@
+import React from "react"; 
+import ReactDOM from "react-dom"; 
+import "./index.css"; 
+import App from "./App"; 
+import CardList from "./components/CardList";
+
+ReactDOM.render(
+<React.StrictMode> 
+  <App />
+<CardList name="shaddam" nim="211201181..." kelompok="0" i sNameBold />
+</React.StrictMode>,
+document.getElementById("root") );
